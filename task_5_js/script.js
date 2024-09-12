014eb22e1d9bd83955e0c4c66b9c7cf8e55db356
Sitaraman S <sitaraman.raghav.india@gmail.com>
2024-09-12 09:12:26 +0530

    Push task 5

--- task_5_js/script.js
+++ task_5_js/script.js
@@ -1,3 +1,24 @@
 function myFunc() {
   document.getElementById("demo").innerHTML = "Hello CCE Again!";
 }
+
+
+function external() {
+  document.getElementById("demo").innerHTML = "Hello External!";
+}
+
+function ifElse() {
+  num = parseInt(document.getElementById("input").value);
+  if (num >= 0) {
+    document.getElementById("demo").innerHTML = "Positive";
+  } else {
+    document.getElementById("demo").innerHTML = "Negative";
+  }
+}
+
+function ternary() {
+  num = parseInt(document.getElementById("input").value);
+  num >= 0
+    ? (document.getElementById("demo").innerHTML = "Positive")
+    : (document.getElementById("demo").innerHTML = "Negative");
+}
